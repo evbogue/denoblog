991ff95bd82402dbc08dea5ecb67785fe92e1554
Ev Bogue <ev@evbogue.com>
2022-07-24 12:52:47 -0500

    render posts and a post list too!

--- blog.js
+++ blog.js
@@ -1,7 +1,8 @@
-import { parse } from "https://deno.land/std@0.149.0/encoding/yaml.ts"
+import { parseAll } from "https://deno.land/std@0.149.0/encoding/yaml.ts"
 import { serveDir } from "https://deno.land/std@0.149.0/http/file_server.ts"
 import { listenAndServe } from "https://deno.land/std/http/server.ts"
-import { walk, walkSync } from "https://deno.land/std@0.149.0/fs/mod.ts";
+import { walk, walkSync } from "https://deno.land/std@0.149.0/fs/mod.ts"
+import { render } from "https://deno.land/x/gfm/mod.ts"
 
 const routes = ['']
 
@@ -9,11 +10,42 @@ const headers = {headers : {"content-type": "text/html; charset=utf-8"}}
 
 let linklist = ''
 
+let postStore = new Map()
+
+let posts = []
+
+let postnames = []
+
+let homepage = {
+  title: 'Posts',
+  markdown: 'List of posts goes here'
+}
+
+
 async function genposts (dir) {
   for await (const entry of walk(dir)) {
     console.log(entry.path)
+    if (entry.name.endsWith('md')) {
+      const post = Deno.readTextFileSync(entry.path)
+      const parsed = parseAll(post)
+      parsed[0].content = parsed[1]
+      const split = entry.name.split('.')
+      parsed[0].markdown = '<p>' + parsed[0].publish_date.toLocaleString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}) + '</p>' + render(parsed[0].content)
+      parsed[0].name = split[0]
+      posts.push(parsed[0])
+      postnames.push(parsed[0].name)
+      postStore.set(parsed[0].name, parsed[0])
+    }
   }
+  let listposts = ''
+  for await (const post of posts) {
+    listposts = listposts + '<li><a href="/' + post.name + '">' + post.title + '</a> — ' + post.publish_date.toLocaleString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}) + '</li>'
+  }
+  homepage.markdown = '<ul>' + listposts + '</ul>'
+  postStore.set('/', homepage)
 }
+
+
 
 function genlinks (links) {
   if (links.length) { 
@@ -23,8 +55,10 @@ function genlinks (links) {
   }
 }
 
-function handle (config, url) {
- 
+function handle (config, postname) {
+  console.log(postname) 
+  const post = postStore.get(postname)
+  console.log(post)
   const head = `
     <html>
       <head>
@@ -39,14 +73,15 @@ function handle (config, url) {
 
   const content = `
     <div class='col three'>
-    <img class='profile' src=${config.avatar || 'https://deno-avatar.deno.dev/avatar/blog.svg'} />
-    <h1>${config.title || 'My Deno Blog'}</h1>
+    <a href="/"><img class='profile' src=${config.avatar || 'https://deno-avatar.deno.dev/avatar/blog.svg'} /></a>
+    <h1><a href="/">${config.title || 'My Deno Blog'}</a></h1>
     <h6>${config.description || 'This is my blog description.'}</h6>
     <hr />
     <ul>${linklist}</ul>
     </div>
     <div class='col eight off-one'>
-      <h2>Posts</h2>
+      <h2>${post.title || 'Posts'}</h2>
+      ${post.markdown || 'Post list should go here'}
     </div>
   `
   return head + content + foot
@@ -57,8 +92,11 @@ export function blog (config) {
   genposts('./posts/')
   listenAndServe(':8000', (req) => {
     const url = new URL(req.url)
+    const postname = url.pathname.substring(1).split('.')[0]
     if (url.pathname == '/index.html' || url.pathname == '/') {
-      return new Response(handle(config, req.url), headers)
+      return new Response(handle(config, '/'), headers)
+    } else if (postnames.includes(postname)) { 
+      return new Response(handle(config, postname), headers)
     } else {
       return serveDir(req, {fsRoot: '', showDirListing: true, quiet: true})
     }
---
+++ posts/next_post.md
@@ -0,0 +1,6 @@
+---
+title: Next Post!
+publish_date: 2022-07-24
+---
+
+This is my next blog post. I am a blogger now!
