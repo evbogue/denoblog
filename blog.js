2a7748248d178c57be9e6c40a403ca282c1a613b
Ev Bogue <ev@evbogue.com>
2022-09-19 12:26:49 -0500

    perhaps this will look better on mobile

--- blog.js
+++ blog.js
@@ -59,6 +59,7 @@ function handle (config, postname) {
     <html>
       <head>
         <title>${config.title || 'My Deno Blog'}</title>
+        <meta name='viewport' content='width=device-width initial-scale=1' />
         <link rel='stylesheet' href='../reserva.css' type='text/css' />
       </head>
       <body style="background: ${config.background || '#fff'};">
