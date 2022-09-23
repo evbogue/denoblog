5d4e525b63a1e5e83c48abab9f81eda7686ff0c4
Ev Bogue <ev@evbogue.com>
2022-09-23 08:53:49 -0500

    link to both bogbooks

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import { blog } from './blog.js'
 
 blog({
   title: "Ev's Blog",
-  avatar: 'https://avatars.githubusercontent.com/u/2468866?v=4',
+  avatar: '../ev.jpg',
   author: 'Everett Bogue',
   description: "Restarting my blogging career one commit at a time",
   background: "#f5f5f5",
   links: [
     { title: 'ev@evbogue.com', url: 'mailto:ev@evbogue.com'},
-    { title: 'Bogbook', url: 'https://denobook.com/#ELwPcMFe0kRF9luXO7qDSKXsiCOuQR27JT23L3gz3AE='} 
+    { title: 'Bogbook 💻', url: 'https://denobook.com/#ELwPcMFe0kRF9luXO7qDSKXsiCOuQR27JT23L3gz3AE='}, 
+    { title: 'Bogbook📱', url: 'https://denobook.com/#exBPL9n3OYcwjz0jH7xArUn5jiOfsa3hLUKF1ucgZok='} 
   ]
 })
