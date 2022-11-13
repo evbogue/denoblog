f31f559f26ce5a5b0bbdc7d5fd1c33080980c608
Ev Bogue <ev@evbogue.com>
2022-11-13 13:15:37 -0600

    Add correct links to bogbook profiles

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ blog({
   background: "#f5f5f5",
   links: [
     { title: 'ev@evbogue.com', url: 'mailto:ev@evbogue.com'},
-    { title: 'Bogbook 💻', url: 'https://denobook.com/#ELwPcMFe0kRF9luXO7qDSKXsiCOuQR27JT23L3gz3AE='}, 
-    { title: 'Bogbook📱', url: 'https://denobook.com/#exBPL9n3OYcwjz0jH7xArUn5jiOfsa3hLUKF1ucgZok='} 
+    { title: 'Bogbook 💻', url: 'https://bogbook.com/#ebAzm6zzxkYHdBynPlDfX9+VyKYYLGz7GIoNi9l4SJ0='}, 
+    { title: 'Bogbook📱', url: 'https://bogbook.com/#ezVThYBx5hoRMnVB1uAF+WT5t9soXvbxUqq0j8tMEZI='} 
   ]
 })
